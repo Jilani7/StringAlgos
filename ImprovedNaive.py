db661ac9892f7076447cbb48c3126cbda82d64ae
Jilani7 <you@example.com>
2020-04-02 15:43:29 +0500

    Initial function

---
+++ ImprovedNaive.py
@@ -0,0 +1,16 @@
+#Algorithm Help taken from internet but i fully understand the code
+
+def ImprovedNaive(String, Pattern):
+    i = 0
+    while i <= len(String) - len(Pattern):
+        for j in range(len(Pattern)):
+            if String [i+j] != Pattern [j]:
+                break
+            j += 1
+        if j == len(Pattern):
+            print ("Found!")
+            i = i + len(Pattern)
+        elif j == 0:
+            i = i + 1
+        else:
+            i = i+ j
