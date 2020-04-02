a651d9c63b6c5e20bb3980828158244ce567e0da
Jilani7 <you@example.com>
2020-04-02 15:52:51 +0500

    Added PiFunction.py

---
+++ PiFunction.py
@@ -0,0 +1,12 @@
+# Algorithm is taken from slides and i code it here I fully understand what code is doing
+
+def PiFunction(p):
+	l = len(p)
+	pi = [0]
+	j = 0
+	for i in range(1,l):
+		while(j > 0 and p[j] != p[i]):
+			j = pi[j-1]
+		if (p[j] == p[i]):
+			j = j + 1
+		pi.append(j)
